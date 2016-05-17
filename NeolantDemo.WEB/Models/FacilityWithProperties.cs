6dc70e2c9099bb6d36ce761be03640bb4d69c1d7
sh-coder <aleksej.babaev@gmail.com>
2016-05-17 10:39:45 +0300

    Реализация метода Equals

--- NeolantDemo.WEB/Models/FacilityWithProperties.cs
+++ NeolantDemo.WEB/Models/FacilityWithProperties.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Runtime.Serialization;
 
 namespace NeolantDemo.WEB.Models
@@ -44,5 +45,43 @@ namespace NeolantDemo.WEB.Models
         /// <value>Список свойств.</value>
         [DataMember(Order = 5)]
         public List<Property> Properties { get; set; }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" /> is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns><c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.</returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+
+            var other = (FacilityWithProperties) obj;
+
+            return InstanceS == other.InstanceS
+                   && ParentInstanceS == other.ParentInstanceS
+                   && KindS == other.KindS
+                   && string.Equals(Identifier, other.Identifier)
+                   && Properties.OrderBy(f => f.PropertyKindS)
+                            .SequenceEqual(other.Properties.OrderBy(s => s.PropertyKindS));
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.</returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = InstanceS.GetHashCode();
+                hashCode = (hashCode * 397) ^ ParentInstanceS.GetHashCode();
+                hashCode = (hashCode * 397) ^ KindS.GetHashCode();
+                hashCode = (hashCode * 397) ^ (Identifier != null ? Identifier.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Properties != null ? Properties.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
     }
 }
--- NeolantDemo.WEB/Models/Property.cs
+++ NeolantDemo.WEB/Models/Property.cs
@@ -83,5 +83,36 @@ namespace NeolantDemo.WEB.Models
         /// </summary>
         [DataMember(Order = Int32.MaxValue)]
         public string StringValue { get; set; }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" /> is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns><c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.</returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            
+            var other = ((Property) obj);
+
+            return PropertyKindS == other.PropertyKindS
+                              && Equals(_dynamicValue, other._dynamicValue);
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.</returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = (_dynamicValue != null ? _dynamicValue.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ PropertyKindS.GetHashCode();
+                return hashCode;
+            }
+        }
     }
 }
